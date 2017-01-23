4abd0527c1a76fc1696f5726ee4cbf35885b85ae
andela-esitati <elsis.sitati@andela.com>
2017-01-23 17:06:45 +0300

    added soup view

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,2 +1,17 @@
 class SoupsController < ApplicationController
+
+	before_action :fetch_soup, :only[:show,:edit, :update,:destroy]
+
+	def show
+
+		respond_to do |format| 
+			format.html
+			format.json {render json: @soup}
+		end
+	end
+
+	private
+	def fetch_soup
+		@soup = Soup.find(params[:id])
+	end
 end
